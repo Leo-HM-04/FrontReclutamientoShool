ebf4a62845dbb0ac0e0748fc2aa2f6fe3d68619d
Leo-HM-04 <leonardohm1904@gmail.com>
2026-03-11 12:30:02 -0600

    campos opcionales en el formulario

--- src/components/clients/ClientForm.tsx
+++ src/components/clients/ClientForm.tsx
@@ -204,7 +204,7 @@ export default function ClientForm({ clientId, onSuccess }: ClientFormProps) {
           <div className="p-6 grid grid-cols-1 md:grid-cols-2 gap-4">
             <div className="md:col-span-2">
               <label className="block text-sm font-medium text-gray-700 mb-2">
-                Nombre de la Empresa *
+                Nombre de la Empresa <span className="text-red-500">*</span>
               </label>
               <input
                 type="text"
